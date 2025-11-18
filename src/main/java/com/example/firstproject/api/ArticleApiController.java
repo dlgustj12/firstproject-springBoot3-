c88899c6ff3a0a7bf6321cd703e4cef36432c8a3
82102 <dlgustj0524@naver.com>
2025-11-18 23:00:50 +0900

    d

--- src/main/java/com/example/firstproject/api/ArticleApiController.java
+++ src/main/java/com/example/firstproject/api/ArticleApiController.java
@@ -3,11 +3,15 @@ package com.example.firstproject.api;
 import com.example.firstproject.dto.ArticleForm;
 import com.example.firstproject.entity.Article;
 import com.example.firstproject.repository.ArticleRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Slf4j
 @RestController
 public class ArticleApiController {
 
@@ -34,6 +38,25 @@ public class ArticleApiController {
     }
 
     //PATCH
+    @PatchMapping("/api/articles/{id}")
+    public ResponseEntity<Article> update(@PathVariable Long id,
+                          @RequestBody ArticleForm dto){
+        //1. DTO -> 엔티티 변환
+        Article article = dto.toEntity();
+        log.info("id: {}, article: {}", id, article.toString());
+
+        //2. 타깃 조회
+        Article target = articleRepository.findById(id).orElse(null);
+
+        //3. 잘못된 요청 처리
+        if(target == null || id != article.getId()){
+            log.info("wrong ask! id: {}, article: {}", id, article.toString());
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
+
+        }
+
+        //4. 업데이트 및 정상 응답(200)하기
+    }
 
 
     //DELETE
